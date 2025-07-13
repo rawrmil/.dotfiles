2256025058e2c8a908ae16eb141b7680ac4cacda
Ramil Hasanshin <khasanshin.ramil@list.ru>
2025-07-13 21:10:14 +0300

    Auto-sync Sun Jul 13 09:10:14 PM MSK 2025

--- scripts/dwm/dwm-loop.sh
+++ scripts/dwm/dwm-loop.sh
@@ -12,11 +12,11 @@ cleanup() {
 trap cleanup INT TERM EXIT
 
 while true; do
-	[ hascmd hsetroot ] && hsetroot -solid black
-	[ hascmd setxkbmap ] && setxkbmap -layout us,ru -option grp:alt_shift_toggle
-	[ hascmd picom ] && picom -b --config ~/dots/dwm/picom.conf
-	[ hascmd flameshot ] && flameshot &
-	[ hascmd xrdb ] && xrdb -merge $HOME/.Xresources
+	hsetroot -solid black
+	setxkbmap -layout us,ru -option grp:alt_shift_toggle
+	picom -b --config ~/dots/dwm/picom.conf
+	flameshot &
+	xrdb -merge $HOME/.Xresources
 	~/.dotfiles/scripts/dwm/dwm-bar.sh &
 	dwm
 	sleep 1
