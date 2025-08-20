256b68ec1435212b612ed87c3b57d2bea4807bd8
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-08-20 19:29:40 +0300

    Auto-sync Wed Aug 20 07:29:40 PM MSK 2025

--- git-config.sh
+++ git-config.sh
@@ -4,6 +4,7 @@ git config --global user.name "Ramil Khasanshin"
 git config --global user.email "khasanshin.ramil@list.ru"
 git config --global alias.a 'add'
 git config --global alias.r 'rm --cached'
+git config --global alias.d 'diff'
 git config --global alias.l 'log --oneline --graph --decorate'
 git config --global alias.s 'status'
 git config --global alias.p 'push'
