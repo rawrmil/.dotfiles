ea4936dede8f03ac30f135181f48e380f2e95b34
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-08-23 14:27:40 +0300

    chore: removed old dwm stuff

--- scripts/dwm/dwm-bar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-xsetroot -name "Wait..."
-while true; do
-	BAT0=`find /sys/devices -name capacity -exec cat {} \;`
-	[ -z $BAT0 ] && BATTERY="INF" || BATTERY="$BAT0%"
-	DATETIME=`date '+%d.%m %H:%M'`
-	xsetroot -name "BAT=$BATTERY | $DATETIME"
-	sleep 30
-done
