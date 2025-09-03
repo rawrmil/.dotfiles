81f41178d25283be0fb183540498d312ebfe5f1e
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-09-04 00:40:02 +0300

    Auto-sync Thu Sep  4 12:40:02 AM MSK 2025

--- scripts/utils/dotfiles-sync.sh
+++ scripts/utils/dotfiles-sync.sh
@@ -1,7 +1,7 @@
-#/bin/bash
-cd ~/.dotfiles
+#!/bin/sh
+cd ~/.dotfiles || exit 1
 echo "[ dotfiles-sync.sh ] BEGIN"
-git pull --rebase
+git pull --no-rebase
 git add .
 git commit -m "Auto-sync $(date)" || true
 git push
